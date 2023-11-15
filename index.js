df475c70ab4b03917954cfc3f192f90e12c807f1
shakib <shakibhossen100@gmail.com>
2023-11-15 14:40:40 +0600

    fix bug

--- index.js
+++ index.js
@@ -14,10 +14,8 @@ const websocket = new WebSocketServer({
 
 websocket.on("request", (request) => {
   const connection = request.accept(null, request.origin);
-  
 
-
-  connection.on("open", e => {
+  connection.on("open", (e) => {
     console.log(`Connection ID (opened): ${connection.remoteAddress}`);
     console.log(`Total Connections: ${connections.size}`); // Log the number of connected clients
   });
@@ -25,7 +23,9 @@ websocket.on("request", (request) => {
     for (const [clientId, conn] of connections) {
       if (conn === connection) {
         connections.delete(clientId);
-        console.log(`Connection closed (total connections: ${connections.size})`);
+        console.log(
+          `Connection closed (total connections: ${connections.size})`
+        );
         break;
       }
     }
@@ -50,10 +50,12 @@ websocket.on("request", (request) => {
   //sendMessageEvery10Seconds(connection)
 });
 
-const serverIp = '192.168.0.109'
-const serverPort = 8080
+const serverIp = "192.168.0.109";
+const serverPort = 8080;
 
-httpServer.listen(serverPort, serverIp, () => console.log("my server is listening"));
+httpServer.listen(serverPort, serverIp, () =>
+  console.log("my server is listening")
+);
 
 // function sendMessageEvery10Seconds(connection) {
 //   setInterval(() => {
@@ -65,57 +67,77 @@ httpServer.listen(serverPort, serverIp, () => console.log("my server is listenin
 //   connection.send(message);
 // }
 
-
-
-function parse( params, connection) {
+function parse(params, connection) {
   console.log(`Raw data: ${params.utf8Data}`);
   try {
-      const parsedMessage = JSON.parse(params.utf8Data);
-      const action = parsedMessage.action;
-      console.log(`p - ${action}`);
-      switch (action) {
-          case 'join':
-              Join(parsedMessage.receiver, connection, parsedMessage.data); // data use a device name
-              break;
-          case 'single':
-              SingleSend(parsedMessage.receiver, parsedMessage.data);
-              break;
-          default:
-              SendALl(parsedMessage.data);
-              break;
-      }
+    const parsedMessage = JSON.parse(params.utf8Data);
+    const action = parsedMessage.action;
+    console.log(`p - ${action}`);
+    switch (action) {
+      case "join":
+        Join(parsedMessage.receiver, connection, parsedMessage.data); // data use a device name
+        break;
+      case "single":
+        SingleSend(parsedMessage.receiver, connection, parsedMessage.data);
+        break;
+      default:
+        SendALl(parsedMessage.data, connection);
+        break;
+    }
   } catch (error) {
-      console.error(`Error parsing JSON: ${error}`);
+    console.error(`Error parsing JSON: ${error}`);
   }
 }
 
-function Join( id , connection, deviceName) {
+function Join(id, connection, deviceName) {
   const newUser = new User(connection, id, deviceName);
   connections.set(id, newUser);
-  sent(newUser, 'Joined successful');
+  sent(newUser, "Joined successful");
   console.log(connections.size);
 }
-function SingleSend( receiverId , message) {
+function SingleSend(receiverId, connection, message) {
+  let senderUser;
   for (const [clientId, user] of connections) {
-    if (clientId === receiverId) {
-      sent(user, message)
-      console.log(`message sent to ${clientId}`);
+    if (user.connection == connection) {
+      senderUser = user;
       break;
     }
   }
+  console.log('sender user')
+  console.log(senderUser);
+  if (senderUser != null)
+    if (connections.has(receiverId)) {
+      const receiverUser = connections.get(receiverId);
+
+      const modifyUser = new User(
+        receiverUser.connection,
+        senderUser.id,
+        senderUser.deviceName
+      );
+      sent(modifyUser, message);
+    }
 }
-function SendALl( message ) {
-  
+function SendALl(message, connection) {
+ let senderUser;
   for (const [clientId, user] of connections) {
-   
-      sent(user, message)
-      console.log(`message sent to ${clientId}`);
-      
-  
+    if (user.connection == connection) {
+      senderUser = user;
+      break;
+    }
   }
+  if (senderUser != null)
+    for (const [clientId, receiverUser] of connections) {
+      const modifyUser = new User(
+        receiverUser.connection,
+        senderUser.id,
+        senderUser.deviceName
+      );
+      sent(modifyUser, message);
+    }
 }
 
-function sent(user,message){
+function sent(user, message) {
+  console.log(user);
   const payload = {
     id: user.id,
     device: user.deviceName,
